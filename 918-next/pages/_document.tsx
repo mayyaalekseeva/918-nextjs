12b54b2a70d69628e240250f6babedb822cbff7f
Mayya Alexeeva <loth@mail.ru>
2023-08-29 23:24:27 +0300

    First commit

---
+++ 918-next/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>9/18 бистро</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Бистро со спешалти кофе" />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

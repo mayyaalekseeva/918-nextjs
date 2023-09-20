7c85d4f793545cb96e7344c0c6f61dc4ce8cd69e
Mayya Alexeeva <loth@mail.ru>
2023-09-20 18:42:44 +0300

    Fix build

--- app/components/navigation/navigation.tsx
+++ app/components/navigation/navigation.tsx
@@ -9,10 +9,11 @@ export default function Navigation() {
     <nav className="">
       <div className="container mx-auto px-6 py-6 border-b-green-100 border-b-4">
         <div className="flex justify-center gap-24 text-2xl uppercase ">
-          {Object.keys(config.pages).map((page) => (
+          {Object.keys(config.pages).map((page, i) => (
             <Link
               href={config.pages[page as keyof typeof config.pages].url}
               className="hover:text-green-500"
+              key={page + i}
             >
               {config.pages[page as keyof typeof config.pages].title}
             </Link>
